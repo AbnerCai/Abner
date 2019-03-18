41603ba14ee092d04d0d2979c30089e04835d882
AbnerCai <335997652@qq.com>
2019-03-18 15:21:35 +0800

    init

--- abner/src/main/java/top/abner/base/BaseActivity.kt
+++ abner/src/main/java/top/abner/base/BaseActivity.kt
@@ -3,6 +3,12 @@ package top.abner.base
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * 
+ * @author Nebula
+ * @version 1.0.0
+ * @date 2019/3/18 15:17
+ */
 abstract class BaseActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
